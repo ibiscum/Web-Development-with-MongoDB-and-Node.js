8f2041bac568f41fc1c79a0ff970be81a83ef807
ibiscum <ulf.luecking@gmail.com>
2025-12-24 17:37:10 +0100

    old

--- ch02/ch02_01_mongodb.js
+++ ch02/ch02_01_mongodb.js
@@ -1,72 +1,97 @@
-var MongoClient = require('mongodb').MongoClient;
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-var dbhost = 'mongodb://localhost:27017/test',
-  myCollection = 'chapter2';
+const uri = 'mongodb://root:password@localhost:27017/ch02-test';
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  }
+);
 
-var seedData = function (db, callback) {
-  db.collection(myCollection).find({}, {}, {})
-    .toArray(
-      function (err, docs) {
-        if (docs.length <= 0) {
-          console.log('No data. Seeding...');
+const myCollection = 'ch02';
 
-          var ihandler = function (err) {
-            if (err) {
-              console.log(err);
-              throw err;
-            }
-            inserted++;
-          }
-
-          var toinsert = 2,
-            inserted = 0;
-
-          db.collection(myCollection).insert({
-            'Title': 'Snow Crash',
-            'Author': 'Neal Stephenson'
-          }, ihandler);
-          db.collection(myCollection).insert({
-            'Title': 'Neuromancer',
-            'Author': 'William Gibson'
-          }, ihandler);
-
-          var sync = setInterval(function () {
-            if (inserted === toinsert) {
-              clearInterval(sync);
-              callback(db);
-            }
-          }, 50);
-          return;
-        }
-        callback(db);
-        return;
-      }
-    );
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+run().catch(console.dir);
 
-var showDocs = function (db) {
-  console.log("Listing books:");
-  var options = {
-    sort: [
-      ['Title', 1]
-    ]
-  };
-  db.collection(myCollection).find({}, {}, options)
-    .toArray(
-      function (err, docs) {
-        if (err) throw err;
+// var seedData = function (db, callback) {
+//   db.collection(myCollection).find({}, {}, {})
+//     .toArray(
+//       function (err, docs) {
+//         if (docs.length <= 0) {
+//           console.log('No data. Seeding...');
 
-        for (var d = 0; d < docs.length; d++) {
-          console.log(docs[d].Title + '; ' + docs[d].Author);
-        }
+//           var ihandler = function (err) {
+//             if (err) {
+//               console.log(err);
+//               throw err;
+//             }
+//             inserted++;
+//           }
 
-        db.close();
-      }
-    );
-}
+//           var toinsert = 2,
+//             inserted = 0;
 
-MongoClient.connect(dbhost, function (err, db) {
-  if (err) throw err;
+//           db.collection(myCollection).insert({
+//             'Title': 'Snow Crash',
+//             'Author': 'Neal Stephenson'
+//           }, ihandler);
+//           db.collection(myCollection).insert({
+//             'Title': 'Neuromancer',
+//             'Author': 'William Gibson'
+//           }, ihandler);
 
-  seedData(db, showDocs);
-});
+//           var sync = setInterval(function () {
+//             if (inserted === toinsert) {
+//               clearInterval(sync);
+//               callback(db);
+//             }
+//           }, 50);
+//           return;
+//         }
+//         callback(db);
+//         return;
+//       }
+//     );
+// }
+
+// var showDocs = function (db) {
+//   console.log("Listing books:");
+//   var options = {
+//     sort: [
+//       ['Title', 1]
+//     ]
+//   };
+//   db.collection(myCollection).find({}, {}, options)
+//     .toArray(
+//       function (err, docs) {
+//         if (err) throw err;
+
+//         for (var d = 0; d < docs.length; d++) {
+//           console.log(docs[d].Title + '; ' + docs[d].Author);
+//         }
+
+//         db.close();
+//       }
+//     );
+// }
+
+// MongoClient.connect(dbhost, function (err, db) {
+//   if (err) throw err;
+
+
+// });
